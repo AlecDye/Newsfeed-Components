4a060b9057a5219a6630f1e4ab491c42f9765e1a
Alec Dye <alecdye@gmail.com>
2020-01-15 16:05:35 -0800

    menu completed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,69 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+// --- function to create new list items ---
+function createMenu(menuItems) {
+  const menu = document.createElement('div');
+  const list = document.createElement('ul');
+  // --- create li and add them to list ---
+  menuItems.forEach(item => {
+    let listItem = document.createElement('li');
+    listItem.textContent = item;
+    list.append(listItem);
+  });
+  menu.append(list);
+  // --- styles ---
+  menu.classList.add('menu');
+  // --- toggle event ---
+  const buttonMenu = document.querySelector('.menu-button');
+  buttonMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu--open');
+  })
+  // --- return function ---
+  return menu;
+};
+
+// --- call the function ---
+const navAnchor = document.querySelector('.header');
+menuItems.forEach(item => {
+  navAnchor.append(createMenu(menuItems));
+})
+
+// function createPopOutMenu(menuItems) {
+
+//   // --- creating elements ---
+//   const menu = document.createElement('div');
+//   const list = document.createElement('ul');
+//   // const listItem = document.createElement('li');
+
+
+//   // --- structure of elements ---
+//   menu.append(list);
+//   // list.append(listItem);
+
+//   // --- styling elements ---
+//   menu.classList.add('menu');
+
+//   // --- ieration ---
+//   menuItem.forEach(item => {
+//     let menuItem = document.querySelector('li');
+//     menuItem.textContent = item;
+//     list.appendChild(menuItem);
+//   });
+
+//   // // -- styling li ---
+//   // menuItem.classList.add('menu ul li');
+
+//   const buttonMenu = document.querySelector('.menu-button');
+//   buttonMenu.addEventListener('click', () => {
+//     menu.classList.toggle('menu--open');
+//   })
+
+//   return menu;
+// };
+
+// const menuImg = document.querySelector('.menu-button');
+// menuItems.forEach(data => {
+//   menuImg.append(createPopOutMenu(menuItems))
+// })
